6e7579cbc84994606267edbf3b2bb9489d41c9a1
BionicCode <vybz@gmx.de>
2019-09-03 16:14:39 +0200

    release

--- BionicLibraryNet/BionicLibraryNet/Properties/AssemblyInfo.cs
+++ BionicLibraryNet/BionicLibraryNet/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,10 +7,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BionicLibraryNet")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Reusable utility and class library based on .NET for WPF")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BionicLibraryNet")]
+[assembly: AssemblyCompany("BionicCode")]
+[assembly: AssemblyProduct("BionicLibrary.Net")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -34,3 +35,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
+
